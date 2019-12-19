70d5ffba14951d415dc836dd0d53cae25111f8b4
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2019-12-19 14:17:07 +0100

    AZ-133: Fix due to diag settings module update

--- r-logs.tf
+++ r-logs.tf
@@ -1,9 +1,9 @@
 module "logging" {
   source = "github.com/claranet/terraform-azurerm-diagnostic-settings.git?ref=multiple-resources"
 
-  enabled        = var.enable_logging
-  resource_ids   = [azurerm_app_service.app_service.id]
-  retention_days = var.logs_storage_retention
+  resources_count = var.enable_logging ? 1 : 0
+  resource_ids    = [azurerm_app_service.app_service.id]
+  retention_days  = var.logs_storage_retention
 
   storage_account_id         = var.logs_storage_account_id
   log_analytics_workspace_id = var.logs_log_analytics_workspace_id
