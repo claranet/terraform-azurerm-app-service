9766f8ae487fab1154ef1374f084366cb883904a
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2020-01-31 15:54:53 +0100

    AZ-121: Pin storage-sas-token module version

--- datasources.tf
+++ datasources.tf
@@ -7,7 +7,7 @@ data "azurerm_app_service_plan" "plan" {
 }
 
 module "backup_sas_token" {
-  source = "git::ssh://git@git.fr.clara.net/claranet/cloudnative/projects/cloud/azure/terraform/modules/storage-sas-token.git?ref=AZ-122-first-version"
+  source = "git::ssh://git@git.fr.clara.net/claranet/cloudnative/projects/cloud/azure/terraform/modules/storage-sas-token.git?ref=v2.0.0"
 
   enabled              = var.enable_backup
   resource_group_name  = var.backup_storage_account_rg
@@ -16,7 +16,7 @@ module "backup_sas_token" {
 }
 
 module "logs_sas_token" {
-  source = "git::ssh://git@git.fr.clara.net/claranet/cloudnative/projects/cloud/azure/terraform/modules/storage-sas-token.git?ref=AZ-122-first-version"
+  source = "git::ssh://git@git.fr.clara.net/claranet/cloudnative/projects/cloud/azure/terraform/modules/storage-sas-token.git?ref=v2.0.0"
 
   enabled              = local.enable_storage_logging
   resource_group_name  = var.logs_storage_account_rg
