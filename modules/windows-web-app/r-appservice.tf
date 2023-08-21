69c23c16286542cc998f1463d49e2b4248e68915
Fabio Santos <fabiodanielmonteirosantos@gmail.com>
2023-08-21 14:43:54 +0100

    Add user assigned identities to missing resources

--- modules/windows-web-app/r-appservice.tf
+++ modules/windows-web-app/r-appservice.tf
@@ -331,8 +331,12 @@ resource "azurerm_windows_web_app_slot" "app_service_windows_slot" {
   client_affinity_enabled = var.client_affinity_enabled
   https_only              = var.https_only
 
-  identity {
-    type = "SystemAssigned"
+  dynamic "identity" {
+    for_each = var.identity[*]
+    content {
+      type         = var.identity.type
+      identity_ids = var.identity.identity_ids
+    }
   }
 
   dynamic "storage_account" {
